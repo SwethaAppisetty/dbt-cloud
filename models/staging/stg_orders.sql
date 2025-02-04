02d67feb087e63e5ae901aa769790b0198f8c094
Swetha Appisetty <swetha.appisetty@anblicks.com>
2025-02-04 09:57:15 +0000

    changed materializations for raw, stg and reporting, add src

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -18,7 +18,7 @@ productname,
 subcategory,
 ordersellingprice - ordercostprice as orderprofit 
 from {{ ref('raw_orders') }} as o 
-left join {{ ref('raw_customers') }} as c 
+left join {{ ref('raw_customer') }} as c 
 on o.customerid = c.customerid
 left join {{ ref('raw_product') }} as p 
 on o.productid = p.productid
